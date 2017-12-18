230b7f9b2efcf03e461240d8ce246c93ce67269d
Broham <abe@spatially.com>
2017-12-18 07:24:15 -0800

    adding file

---
+++ polygon.go
@@ -0,0 +1,17 @@
+package Circ2Poly
+
+import (
+	"fmt"
+	"github.com/StefanSchroeder/Golang-Ellipsoid/ellipsoid"
+)
+
+func ApproximateWKTCircle(srcLat float64, srcLong float64, radius float64, sections float64) string {
+	geo := ellipsoid.Init("WGS84", ellipsoid.Degrees, ellipsoid.Meter, ellipsoid.LongitudeIsSymmetric, ellipsoid.BearingNotSymmetric)
+	points := make([]string, int(0))
+	for i := 0.0; i <= sections; i++ {
+		adjust := i / sections
+		lat, long := geo.At(srcLat, srcLong, radius, adjust*360.0)
+		points = append(points, fmt.Sprintf("%v %v", long, lat))
+	}
+	return fmt.Sprintf("POLYGON((%v))", strings.Join(points, ","))
+}
--- test.go
+++ test.go
@@ -1,7 +1,20 @@
-package test
+package main
 
-import "github.com/Broham/Circ2Poly"
+import (
+	"fmt"
+	"github.com/broham/Circ2Poly/polygon"
+	"strings"
+)
+
+var (
+	geo      = ellipsoid.Init("WGS84", ellipsoid.Degrees, ellipsoid.Meter, ellipsoid.LongitudeIsSymmetric, ellipsoid.BearingNotSymmetric)
+	srcLat   = 29.8477935791016
+	srcLong  = -97.9479217529297
+	radius   = 2000.0
+	sections = 10.0
+)
 
 func main() {
-	circleToPolygon.CircleToPolygon()
+	points := polygon.ApproximateWKTCircle(srcLat, srcLong, radius, sections)
+	fmt.Println("Points:", points)
 }
